79c0390a7c38ac35241846ac5fff51e308cccfa3
Antony Hwang <AntonyHwang@hotmail.com>
2018-01-25 09:52:24 +0000

    first commit

---
+++ cmake-build-debug/CMakeFiles/ICP_Alignment.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ICP_Alignment.dir/src/main.cpp.o"
+  "ICP_Alignment.pdb"
+  "ICP_Alignment"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ICP_Alignment.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
